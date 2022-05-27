d850cb593c68a41896281197ee7f6f517c3fe138
shawn28e7 <95568939+shawn28e7@users.noreply.github.com>
2022-05-27 18:27:13 +0800

    Update main.cpp

--- real_time/main.cpp
+++ real_time/main.cpp
@@ -20,7 +20,6 @@ void process(Mat frame)
     face_cascade.detectMultiScale(frame_gray, faces, 1.1, 5, CV_HAAR_SCALE_IMAGE, Size(30, 30));
     for (size_t i = 0; i < faces.size(); i++)
     {
-        Point center;
         Mat faceROI = frame_gray(faces[i]);
         String cover = "face2.png";
         Mat signal = imread(cover), mask = imread(cover, 0);
@@ -28,10 +27,8 @@ void process(Mat frame)
         w = faces[i].width;
         h = faces[i].height;
         size_t posx, posy;
-        size_t k = 7;
         posx = std::max(1, faces[i].x);
         posy = std::max(1, faces[i].y);
-
         resize(signal, signal, Size(w, h), INTER_LINEAR);
         resize(mask, mask, Size(w, h), INTER_LINEAR);
         Rect rect(0, 0, std::min(w, frame.rows - posx - 1), std::min(h, frame.cols - posy - 1));
