5f872942f837514c91903cf4333061fb0959bc3c
shawn28e7 <95568939+shawn28e7@users.noreply.github.com>
2022-05-29 21:21:26 +0800

    Update main.cpp

--- for_single_picture_like_me/main.cpp
+++ for_single_picture_like_me/main.cpp
@@ -4,7 +4,9 @@
 
 using namespace std;
 using namespace cv;
-#define ERROR cout << "Error on line " << __LINE__ << endl; system("pause");
+#define ERROR                                     \
+    cerr << "Error on line " << __LINE__ << endl; \
+    system("pause");
 
 String face_cascade_name = "C:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml";
 CascadeClassifier face_cascade;
@@ -20,13 +22,15 @@ void process(Mat frame)
     face_cascade.detectMultiScale(frame_gray, faces, 1.1, 5, CV_HAAR_SCALE_IMAGE, Size(30, 30));
     for (size_t i = 0; i < faces.size(); i++)
     {
+        Point center;
         Mat faceROI = frame_gray(faces[i]);
-        String cover = "face2.png"; // your "mask" goes here
+        String cover = "248596880_243152237800419_5280981397072697475_n.jpg"; // your "mask" goes here
         Mat signal = imread(cover), mask = imread(cover, 0);
         size_t w, h;
         w = faces[i].width;
         h = faces[i].height;
         size_t posx, posy;
+        size_t k = 7;
         posx = std::max(1, faces[i].x);
         posy = std::max(1, faces[i].y);
         resize(signal, signal, Size(w, h), INTER_LINEAR);
@@ -35,7 +39,6 @@ void process(Mat frame)
         w = std::min(w, frame.rows - posx - 1);
         h = std::min(h, frame.cols - posy - 1);
         Mat imageROI = frame(Rect(posx, posy, w, h));
-        
         signal.copyTo(imageROI, mask);
     }
     imshow("Face Detection", frame);
@@ -43,16 +46,19 @@ void process(Mat frame)
 }
 
 int main(int argc, char *argv[])
-{ 
+{
     cout << "wait for it" << endl;
     if (!face_cascade.load(face_cascade_name))
+    {
         ERROR;
-    
+    }
     Mat frame;
-    String Target = "3206612_1_1.jpg";
+    String Target = "3323502.jpg";
     frame = imread(Target);
     if (frame.empty())
+    {
         ERROR;
+    }
     process(frame);
     return 0;
 }
