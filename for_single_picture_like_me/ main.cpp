4afca2a66855a66ce69346a1aa1b80a1a6c55868
shawn28e7 <95568939+shawn28e7@users.noreply.github.com>
2022-05-30 11:53:46 +0800

    Update and rename main.cpp to  main.cpp

--- for_single_picture_like_me/ main.cpp
+++ for_single_picture_like_me/ main.cpp
@@ -1,62 +1,61 @@
+/** FaceDetection.cpp **/
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <stdio.h>
 
 using namespace std;
 using namespace cv;
-#define ERROR                                     \
-    cerr << "Error on line " << __LINE__ << endl; \
-    system("pause");
+#define ERROR cout << "Error on line " << __LINE__ << endl;
 
 String face_cascade_name = "C:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_default.xml";
 CascadeClassifier face_cascade;
-String window_name = "result";
+String window_name = "mosaic-please";
 
 void process(Mat frame)
 {
     std::vector<Rect> faces;
     Mat frame_gray;
-
     cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
     equalizeHist(frame_gray, frame_gray);
     face_cascade.detectMultiScale(frame_gray, faces, 1.1, 5, CV_HAAR_SCALE_IMAGE, Size(30, 30));
+
     for (size_t i = 0; i < faces.size(); i++)
     {
+        Point center;
         Mat faceROI = frame_gray(faces[i]);
-        String cover = "248596880_243152237800419_5280981397072697475_n.jpg"; // your "mask" goes here
+        String cover = "face2.png"; // your "mask" goes here
         Mat signal = imread(cover), mask = imread(cover, 0);
         size_t w, h;
         w = faces[i].width;
         h = faces[i].height;
         size_t posx, posy;
+        size_t k = 7;
         posx = std::max(1, faces[i].x);
         posy = std::max(1, faces[i].y);
         resize(signal, signal, Size(w, h), INTER_LINEAR);
         resize(mask, mask, Size(w, h), INTER_LINEAR);
         Rect rect(0, 0, std::min(w, frame.rows - posx - 1), std::min(h, frame.cols - posy - 1));
+        signal = signal(rect);
+        mask = mask(rect);
         w = std::min(w, frame.rows - posx - 1);
         h = std::min(h, frame.cols - posy - 1);
         Mat imageROI = frame(Rect(posx, posy, w, h));
         signal.copyTo(imageROI, mask);
     }
-    imshow("Face Detection", frame);
-    waitKey(0);
+    imshow(window_name, frame);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "wait for it" << endl;
-    if (!face_cascade.load(face_cascade_name))
-    {
-        ERROR;
-    }
     Mat frame;
-    String Target = "3323502.jpg"; //Target picture here
+    if (!face_cascade.load(face_cascade_name))
+        ERROR;
+    cout << "wait for it" << endl;
+    String Target = "2.jpg";
     frame = imread(Target);
     if (frame.empty())
-    {
         ERROR;
-    }
     process(frame);
+    waitKey(0);
     return 0;
 }
